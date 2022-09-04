22ebac3e385b629f74d8dba673f263d3f6446ca6
callus-corn <mtfm_ymzk@yahoo.com>
2022-09-04 11:17:48 +0900

    Fix universal

--- random_mac.py
+++ random_mac.py
@@ -4,7 +4,8 @@ mac_src = []
 for i in range(6):
     mac_byte = random.randint(0,0xff)
     if i==0:
-        mac_byte = mac_byte & 0b1111_1110
+        mac_byte = mac_byte & 0b1111_1110  # Not multicast
+        mac_byte = mac_byte | 0b0000_0010  # Not universal
     mac_src.append(format(mac_byte, '02x'))
 mac = ':'.join(mac_src)
 print(mac)
